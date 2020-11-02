9716ab274ba74d8bb0b36d921f0ed200fd09b4da
nayel-akeb <nayel.akeb@laplateforme.io>
2020-11-02 13:07:41 +0100

    bon it's time to avancer plus vite

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,10 +1,21 @@
 <?php
 $str = "LaPlateforme ";
-
+echo $str. '<br>';
 $str2 = "Vive ";
-
+echo $str2. '<br>';
 $str3 = "!";
+echo $str3. '<br>';
+
+$val = 6;
 
 echo $str2;
 echo $str;
-echo $str3;
+echo $str3. '<br>';
+
+echo $val. '<br>';
+$val =$val + 4;
+echo $val. '<br>';
+$myBool = true;
+echo $myBool. '<br>';
+$myBool = false;
+echo $myBool;
