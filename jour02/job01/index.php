5eaf29825d07fb6b90d615516abe5de5e8f244c5
nayel-akeb <nayel.akeb@laplateforme.io>
2020-11-03 10:19:18 +0100

    bon bah finalement le collège ma servis

---
+++ jour02/job01/index.php
@@ -0,0 +1,18 @@
+<?php
+$nombre = 0;
+$goal = 1337;
+$lavie = 42;
+while ($nombre <= $goal) {
+
+    echo $nombre.'<br>';
+
+    if($nombre == $lavie)
+    { 
+        echo '<b>'.'<u>' .$nombre . '</b>'.'</u>'.'<br>';
+        $nombre = $nombre+1;
+    }
+    else {
+        echo $nombre.'<br>';
+        $nombre = $nombre+1;
+    }
+}
